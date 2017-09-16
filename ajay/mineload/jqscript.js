72e222b5621ee9b299e006f00debe489c8076dc1
ajay <aajay0288@gmail.com>
2017-09-16 15:04:10 +0530

    mynewload

---
+++ ajay/mineload/jqscript.js
@@ -0,0 +1,109 @@
+$(document).ready(function(){
+    $(".cout").click(function(){
+        $(".cout").hide();
+        $("#c0").show();
+    });
+});
+$(document).ready(function(){
+    $(".cout1").click(function(){
+        $(".cout1").hide();
+        $("#c1").show();
+    });
+});
+$(document).ready(function(){
+    $(".cout2").click(function(){
+        $(".cout2").hide();
+        $("#c2").show();
+    });
+});
+$(document).ready(function(){
+    $(".cout3").click(function(){
+        $(".cout3").hide();
+        $("#c3").show();
+    });
+});
+$(document).ready(function(){
+    $(".cout4").click(function(){
+        $(".cout4").hide();
+        $("#c4").show();
+    });
+});
+$(document).ready(function(){
+    $(".cout5").click(function(){
+        $(".cout5").hide();
+        $("#c5").show();
+    });
+});
+$(document).ready(function(){
+    $(".cout6").click(function(){
+        $(".cout6").hide();
+        $("#c6").show();
+    });
+});
+$(document).ready(function(){
+    $(".cout7").click(function(){
+        $(".cout7").hide();
+        $("#c7").show();
+    });
+});
+$(document).ready(function(){
+    $(".cout8").click(function(){
+        $(".cout8").hide();
+        $("#c8").show();
+    });
+});
+
+$(document).ready(function(){
+    $("#c0").click(function(){
+        $("#c0").hide();
+        $(".cout").show();
+    });
+});
+$(document).ready(function(){
+    $("#c1").click(function(){
+        $("#c1").hide();
+        $(".cout1").show();
+    });
+});
+$(document).ready(function(){
+    $("#c2").click(function(){
+        $("#c2").hide();
+        $(".cout2").show();
+    });
+});
+$(document).ready(function(){
+    $("#c3").click(function(){
+        $("#c3").hide();
+        $(".cout3").show();
+    });
+});
+$(document).ready(function(){
+    $("#c4").click(function(){
+        $("#c4").hide();
+        $(".cout4").show();
+    });
+});
+$(document).ready(function(){
+    $("#c5").click(function(){
+        $("#c5").hide();
+        $(".cout5").show();
+    });
+});
+$(document).ready(function(){
+    $("#c6").click(function(){
+        $("#c6").hide();
+        $(".cout6").show();
+    });
+});
+$(document).ready(function(){
+    $("#c7").click(function(){
+        $("#c7").hide();
+        $(".cout7").show();
+    });
+});
+$(document).ready(function(){
+    $("#c8").click(function(){
+        $("#c8").hide();
+        $(".cout8").show();
+    });
+});
